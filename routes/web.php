26c5727afd396271b43b2386d50a0489460b271a
Md Rakibul Islam <r4k1b.m4r5@gmail.com>
2024-06-01 19:41:30 +0600

    Remove dashboard, welcome page & refactor routes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect()->route('tasks.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
