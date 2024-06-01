a5c4a8e7041bf4489332fe548c9f87f19a27eb29
Md Rakibul Islam <r4k1b.m4r5@gmail.com>
2024-06-01 21:39:45 +0600

    login.blade.php: add register link & refactor

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,14 +13,22 @@
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="mt-4 flex items-center justify-between">
+            <div>
+                <x-input-label for="password" :value="__('Password')" />
+            </div>
+            @if (Route::has('password.request'))
+                <a class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+        </div>
 
+        <div class="mt-1">
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
-
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -33,11 +41,9 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                {{ __('Create an account') }}
+            </a>
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
